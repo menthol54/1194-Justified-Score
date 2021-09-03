ff7932cbb93839fa655b6e4f7841583a26291109
Jámin Johnson <menthol54@protonmail.com>
2021-09-03 17:48:23 -0400

    d

--- calc.py
+++ calc.py
@@ -9,29 +9,29 @@ def add():
             print("Are you serious?")
             return
         if args == '2':
-            n = int(input("First Number: "))
+            n = float(input("First Number: "))
             time.sleep(0.5)
-            n1 = int(input("Second Number: "))
-            sum = int(n + n1)
+            n1 = float(input("Second Number: "))
+            sum = float(n + n1)
             print(f'{n} + {n1} = {sum}')
             break
         if args == '3':
-            n = int(input("First Number: "))
+            n = float(input("First Number: "))
             time.sleep(0.5)
-            n1 = int(input("Second Number: "))
+            n1 = float(input("Second Number: "))
             time.sleep(0.5)
-            n2 = int(input("Third Number: "))
-            sum = int(n + n1 + n2)
+            n2 = float(input("Third Number: "))
+            sum = float(n + n1 + n2)
             print(f'{n} + {n1} + {n2} = {sum}')
             break
         if args == '4':
-            n = int(input("First Number: "))
+            n = float(input("First Number: "))
             time.sleep(0.5)
-            n1 = int(input("Second Number: "))
+            n1 = float(input("Second Number: "))
             time.sleep(0.5)
-            n2 = int(input("Third Number: "))
+            n2 = float(input("Third Number: "))
             time.sleep(0.5)
-            n3 = int(input("Fourth Number: "))
-            sum = int(n + n1 + n2 + n3)
+            n3 = float(input("Fourth Number: "))
+            sum = float(n + n1 + n2 + n3)
             print(f'{n} + {n1} + {n2} + {n3} = {sum}')
             break
