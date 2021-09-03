acb4e47fc7fd30dc3d7a24336b45f4921bee3faa
Jámin Johnson <menthol54@protonmail.com>
2021-09-03 18:25:02 -0400

    d

--- calc.py
+++ calc.py
@@ -35,3 +35,37 @@ def add():
             sum = float(n + n1 + n2 + n3)
             print(f'{n} + {n1} + {n2} + {n3} = {sum}')
             break
+def sub():
+    print("How many numbers?")
+    args = input('')
+    while True:
+        if args == '1':
+            print("Are you serious?")
+            return
+        if args == '2':
+            n = float(input("First Number: "))
+            time.sleep(0.5)
+            n1 = float(input("Second Number: "))
+            sum = float(n - n1)
+            print(f'{n} - {n1} = {sum}')
+            break
+        if args == '3':
+            n = float(input("First Number: "))
+            time.sleep(0.5)
+            n1 = float(input("Second Number: "))
+            time.sleep(0.5)
+            n2 = float(input("Third Number: "))
+            sum = float(n - n1 - n2)
+            print(f'{n} - {n1} - {n2} = {sum}')
+            break
+        if args == '4':
+            n = float(input("First Number: "))
+            time.sleep(0.5)
+            n1 = float(input("Second Number: "))
+            time.sleep(0.5)
+            n2 = float(input("Third Number: "))
+            time.sleep(0.5)
+            n3 = float(input("Fourth Number: "))
+            sum = float(n - n1 - n2 - n3)
+            print(f'{n} - {n1} - {n2} - {n3} = {sum}')
+            break
--- main.py
+++ main.py
@@ -6,15 +6,31 @@ greet = ["Hello. My name is 1194 Justified Score", "1194 online"]
 print(random.choice(greet))
 time.sleep(0.5)
 
-def math():
+def func():
     ask = input("What function would you like to access? ")
+    #----------Math Funcs--------#
     if ask == 'math':
         m_func = input("What operation? ")
         if m_func == 'add':
             c.add()
         if m_func == 'sub':
             c.sub()
+        if m_func == 'mul' or 'times':
+            c.times()
+        if m_func == 'div':
+            c.div()
+    if ask == 'help':
+        req = input("""
+Please be more specific? Help math? Help science? 
+Please say 'help' beside the subject.
+Subjects: Math.
+Tpye nothing to return """)
+        if req == '':
+            return ask
+        
+        elif req == 'help math':
+            print('ok')
+
 def main():
-    math()
-if __name__ == '__main__':
-    main()
+    func()
+main()
