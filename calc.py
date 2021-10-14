8dd0469444230f90e0930dea2b902a0586ab6be0
Jámin Johnson <menthol54@protonmail.com>
2021-10-14 15:00:30 -0400

    d

--- calc.py
+++ calc.py
@@ -1,120 +1,62 @@
-import math
-import time
+
 
 def add():
-    print("How many numbers?")
-    args = input('')
-    while True:
-        if args == '1':
-            print("Are you serious?")
-            return
-        if args == '2':
-            n = float(input("First Number: "))
-            time.sleep(0.5)
-            n1 = float(input("Second Number: "))
-            sum = float(n + n1)
-            print(f'{n} + {n1} = {sum}')
-            break
-        if args == '3':
-            n = float(input("First Number: "))
-            time.sleep(0.5)
-            n1 = float(input("Second Number: "))
-            time.sleep(0.5)
-            n2 = float(input("Third Number: "))
-            sum = float(n + n1 + n2)
-            print(f'{n} + {n1} + {n2} = {sum}')
-            break
-        if args == '4':
-            n = float(input("First Number: "))
-            time.sleep(0.5)
-            n1 = float(input("Second Number: "))
-            time.sleep(0.5)
-            n2 = float(input("Third Number: "))
-            time.sleep(0.5)
-            n3 = float(input("Fourth Number: "))
-            sum = float(n + n1 + n2 + n3)
-            print(f'{n} + {n1} + {n2} + {n3} = {sum}')
-            break
+    args = input("Enter all the numbers to be added, separated by a space:\n")
+
+    args = args.split()
+
+    try:
+        end_value = int(args[0])
+        for x in args[1:]:
+            end_value += int(x)
+    except ValueError:
+        print("Improper value, only use numbers...")
         return add()
+
+    print(end_value)
+
+
 def sub():
-    print("How many numbers?")
-    args = input('')
-    while True:
-        if args == '1':
-            print("Are you serious?")
-        if args == '2':
-            n = float(input("First Number: "))
-            time.sleep(0.5)
-            n1 = float(input("Second Number: "))
-            sum = float(n - n1)
-            print(f'{n} - {n1} = {sum}')
-            break
-        if args == '3':
-            n = float(input("First Number: "))
-            time.sleep(0.5)
-            n1 = float(input("Second Number: "))
-            time.sleep(0.5)
-            n2 = float(input("Third Number: "))
-            sum = float(n - n1 - n2)
-            print(f'{n} - {n1} - {n2} = {sum}')
-            break
-        if args == '4':
-            n = float(input("First Number: "))
-            time.sleep(0.5)
-            n1 = float(input("Second Number: "))
-            time.sleep(0.5)
-            n2 = float(input("Third Number: "))
-            time.sleep(0.5)
-            n3 = float(input("Fourth Number: "))
-            sum = float(n - n1 - n2 - n3)
-            print(f'{n} - {n1} - {n2} - {n3} = {sum}')
-            break
+    args = input("Enter all the numbers to be subtracted, separated by a space:\n")
+
+    args = args.split()
+
+    try:
+        end_value = int(args[0])
+        for x in args[1:]:
+            end_value -= int(x)
+    except ValueError:
+        print("Improper value, only use numbers...")
         return sub()
+
+    print(end_value)
+
+
 def times():
-    print("How many numbers?")
-    args = input('')
-    while True:
-        if args == '1':
-            print("Are you serious?")
-            return
-        if args == '2':
-            n = float(input("First Number: "))
-            time.sleep(0.5)
-            n1 = float(input("Second Number: "))
-            sum = float(n + n1)
-            print(f'{n} + {n1} = {sum}')
-            break
-        if args == '3':
-            n = float(input("First Number: "))
-            time.sleep(0.5)
-            n1 = float(input("Second Number: "))
-            time.sleep(0.5)
-            n2 = float(input("Third Number: "))
-            sum = float(n + n1 + n2)
-            print(f'{n} + {n1} + {n2} = {sum}')
-            break
-        if args == '4':
-            n = float(input("First Number: "))
-            time.sleep(0.5)
-            n1 = float(input("Second Number: "))
-            time.sleep(0.5)
-            n2 = float(input("Third Number: "))
-            time.sleep(0.5)
-            n3 = float(input("Fourth Number: "))
-            sum = float(n + n1 + n2 + n3)
-            print(f'{n} + {n1} + {n2} + {n3} = {sum}')
-            break
+    args = input("Enter all the numbers to be multiplied, separated by a space:\n")
+
+    args = args.split()
+
+    try:
+        end_value = int(args[0])
+        for x in args[1:]:
+            end_value *= int(x)
+    except ValueError:
+        print("Improper value, only use numbers...")
         return times()
+    print(end_value)
+
+
 def div():
-    print("How many numbers?")
-    args = input('')
-    while True:
-        if type(args) != int:
-            return args
-        if type(args) == int:
-            n = float(input("First Number:"))
-            n1 = float(input("Second Number: "))
-            sum = float(n / n1)
-            print(f'{n} divided by {n1} = {sum}')
-            break
+    args = input("Enter all the numbers to be divided, separated by a space:\n")
+
+    args = args.split()
+
+    try:
+        end_value = int(args[0])
+        for x in args[1:]:
+            end_value /= int(x)
+    except ValueError:
+        print("Improper value, only use numbers...")
         return div()
+    print(end_value)
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ greet = ["Hello. My name is 1194 Justified Score", "1194 online"]
 print(random.choice(greet))
 time.sleep(0.5)
 
+
 def func():
     #----------Math Funcs--------#
     m_func = input("What operation? ")
@@ -13,12 +14,18 @@ def func():
         c.add()
     elif m_func == 'sub':
         c.sub()
-    elif m_func == 'mul' or 'times':
+    elif m_func == 'mul' or m_func == 'times':
         c.times()
     elif m_func == 'div':
         c.div()
     elif m_func == 'sqrt':
         c.rad()
+
+
 def main():
     func()
+
+
 main()
+
+input('')
